56931335feb1e53ad0ac430ecf53705ffeac2663
Ashish <ashishsaud498@gmail.com>
2025-06-02 01:24:01 +0545

    add reducers in counterSlice

--- redux/src/features/Slice.jsx
+++ redux/src/features/Slice.jsx
@@ -5,7 +5,17 @@ const counterSlice = createSlice({
   initialState: {
     value : 0,
   },
-  reducers: {}
+  reducers: {
+    increment: (state) => {
+      state.value = state.value + 1;
+    },
+    decrement: (state) => {
+      state.value = state.value - 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    }
+  }
 })
 
 export const { increment, decrement, incrementByAmount} = counterSlice.actions;
