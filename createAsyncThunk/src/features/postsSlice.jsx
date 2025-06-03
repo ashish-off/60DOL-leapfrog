9c2253803e268135e667844c4ffcffba78e13076
Ashish <ashishsaud498@gmail.com>
2025-06-03 12:27:32 +0545

    Add CRUD operations for posts: update, delete, and create

--- createAsyncThunk/src/features/postsSlice.jsx
+++ createAsyncThunk/src/features/postsSlice.jsx
@@ -15,6 +15,56 @@ export const fetchPost = createAsyncThunk(
   }
 );
 
+
+// update post
+export const updatePost = createAsyncThunk(
+  "posts/updatePost",
+  async ({ id, updatedData }, { rejectWithValue }) => {
+    try {
+      const res = await API.put(`/${id}`, updatedData);
+      return res.data;
+    } catch (error) {
+      console.error("Error updating post:", error);
+      return rejectWithValue(error.message || "Failed to update post");
+    }
+  }
+);
+
+// delete post
+export const deletePost = createAsyncThunk(
+  "posts/deletePost",
+  async (id, { rejectWithValue }) => {
+    try {
+      await API.delete(`${id}`);
+      return { id };
+    } catch (error) {
+      console.error("Error deleting post : ", error);
+      return rejectWithValue(error.message || "Failed to delete post");
+    }
+  }
+);
+
+// create posts
+
+export const createPost = createAsyncThunk(
+  "posts/createPost",
+  async (newPost, { rejectWithValue }) => {
+    try {
+      const res = await API.post("/", newPost);
+      const data = res.data;
+
+      // JSONPlaceholder returns id: 101 always so we overwrite it with unique local ID
+      return {
+        ...data,
+        id: Date.now(),
+      };
+    } catch (error) {
+      console.error("Error creating post:", error);
+      return rejectWithValue(error.message || "Failed to create post");
+    }
+  }
+);
+
 const postsSlice = createSlice({
   name: "posts",
   initialState: {
