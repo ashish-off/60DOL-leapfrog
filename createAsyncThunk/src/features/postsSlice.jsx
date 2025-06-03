4ee6293dc678ca76cf687a078dfe4a49d5b9ec3d
Ashish <ashishsaud498@gmail.com>
2025-06-03 11:56:49 +0545

    get post in createAsyncThunk

--- createAsyncThunk/src/features/postsSlice.jsx
+++ createAsyncThunk/src/features/postsSlice.jsx
@@ -1,5 +1,20 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+// get posts from API
+export const fetchPost = createAsyncThunk(
+  "posts/fetchPost",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!res.ok) throw new Error("failed to fetch posts");
+      const data = await res.json();
+      return data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message || "Failed to fetch posts");
+    }
+  }
+);
+
 const postsSlice = createSlice({
   name: "posts",
   initialState: {
