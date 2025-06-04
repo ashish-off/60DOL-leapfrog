c068723a2b39228c0f9fee1efed2ac099c62e035
Ashish <ashishsaud498@gmail.com>
2025-06-04 20:31:10 +0545

    React hook form introduction

---
+++ rhf-demo/src/Components/YoutubeForm.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import {DevTool} from '@hookform/devtools'
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+}
+
+const YoutubeForm = () => {
+  const form =  useForm<FormValues>()
+  const {register, control, handleSubmit, } =form
+
+  const onSubmit = (data : FormValues) => {
+    console.log("Form submitted", data)
+  }
+  
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate >
+        <label htmlFor="username">Username</label>
+        <input type="text " id='username' {...register("username", {required :{
+          value : true,
+          message : "username is required"
+        } })} />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id='email' {...register("email")} />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id='channel' {...register("channel")} />
+
+        <button >Submit</button>
+      </form>
+      <DevTool control={control} />
+    </div>
+  )
+}
+
+export default YoutubeForm
