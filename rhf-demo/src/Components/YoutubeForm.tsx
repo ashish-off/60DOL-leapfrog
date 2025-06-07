5c326ceab526f523b3bd10bde9b154039cc0ab90
Ashish <ashishsaud498@gmail.com>
2025-06-07 18:01:27 +0545

    add default value as array , objects and dynamic array

--- rhf-demo/src/Components/YoutubeForm.tsx
+++ rhf-demo/src/Components/YoutubeForm.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 type FormValues = {
   username: string;
   email: string;
   channel: string;
+  socials: {
+    twitter: string;
+    facebook: string;
+  };
+  phoneNumbers: string[];
+  phNumbers: { number: string }[];
+  age: number;
 };
 
 const YoutubeForm = () => {
@@ -14,8 +21,16 @@ const YoutubeForm = () => {
       username: "",
       email: "",
       channel: "",
-    }
+      socials: {
+        twitter: "",
+        facebook: "",
+      },
+      phoneNumbers: ["", ""], // initiallizing phoneNumbers (2 fields)
+      phNumbers: [{ number: "" }],
+      age: 0,
+    },
   });
+
   const { register, control, handleSubmit, formState } = form;
   // register is a function that registers the input field with the form
   // control is used to control the form state and validation
@@ -23,6 +38,13 @@ const YoutubeForm = () => {
   // formState contains the state of the form, like errors, isValid, isDirty, etc.
   const { errors } = formState;
   // error have types as requierd, pattern, minLength .. etc
+
+  // useFieldArray is used to manage an array of fields in the form
+  const { fields, append, remove } = useFieldArray({
+    name: "phNumbers",
+    control,
+  });
+
   const onSubmit = (data: FormValues) => {
     console.log("Form submitted", data);
   };
@@ -31,10 +53,10 @@ const YoutubeForm = () => {
     <div>
       <h1>An Youtube Form</h1>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <label htmlFor="username">Username</label>
-
-        {/* register function's first argument is the field name, and the second argument is the validation rules */}
         <div className="form-control">
+          <label htmlFor="username">Username</label>
+
+          {/* register function's first argument is the field name, and the second argument is the validation rules */}
           <input
             type="text "
             id="username"
@@ -50,8 +72,8 @@ const YoutubeForm = () => {
           )}
         </div>
 
-        <label htmlFor="email">Email</label>
         <div className="form-control">
+          <label htmlFor="email">Email</label>
           <input
             type="email"
             id="email"
@@ -66,8 +88,8 @@ const YoutubeForm = () => {
               },
 
               // custom validation is created using validate function
-              validate:{
-                noAdmin :  (fieldValue) => {
+              validate: {
+                noAdmin: (fieldValue) => {
                   // Custom validation function
                   if (fieldValue.includes("admin")) {
                     return "Email cannot contain 'admin'";
@@ -75,17 +97,19 @@ const YoutubeForm = () => {
                   return true; // Return true if validation passes
                 },
                 notBlackList: (fieldName) => {
-                  return !fieldName.endsWith("@blacklist.com")
-                    || "This email is blacklisted"
-                }
+                  return (
+                    !fieldName.endsWith("@blacklist.com") ||
+                    "This email is blacklisted"
+                  );
+                },
               },
             })}
           />
           {errors.email && <p className="error">{errors.email.message}</p>}
         </div>
 
-        <label htmlFor="channel">Channel</label>
         <div className="form-control">
+          <label htmlFor="channel">Channel</label>
           <input
             type="text"
             id="channel"
@@ -99,6 +123,77 @@ const YoutubeForm = () => {
           <p className="error">{errors.channel?.message}</p>
         </div>
 
+        <div className="form-control">
+          <label htmlFor="twitter">twitter</label>
+          <input type="text" id="twitter" {...register("socials.twitter")} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">facebook</label>
+          <input type="text" id="facebook" {...register("socials.facebook")} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="primary-phone">primary phone number</label>
+          <input
+            type="text"
+            id="primary-phone"
+            {...register("phoneNumbers.0")}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondary-phone">secondary phone number</label>
+          <input
+            type="text"
+            id="secondary-phone"
+            {...register("phoneNumbers.1")}
+          />
+        </div>
+
+        <div>
+          <label>List of phone numbers</label>
+          <div>
+            {fields.map((field, index) => (
+              <div className="form-control" key={field.id}>
+                <input
+                  type="text"
+                  {...register(`phNumbers.${index}.number` as const)}
+                />
+                {index > 0 && (
+                  <button type="button" onClick={() => remove(index)}>
+                    remove
+                  </button>
+                )}
+              </div>
+            ))}
+            <button type="button" onClick={() => append({ number: "" })}>
+              Add phone number
+            </button>
+          </div>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="age">age</label>
+          <input
+            type="number"
+            id="age"
+            {...register("age", {
+              required: {
+                value: true,
+                message: "Age is required",
+              },
+              valueAsNumber: true, // note: use valueAsDate for date inputs
+              validate : {
+                notPositive: (value) => {
+                  return value > 0 || "Age must be a positive number";
+                }
+              } // to convert the input value to a number
+            })}
+          />
+          <p className="error">{errors.age?.message}</p>
+        </div>
+
         <button>Submit</button>
       </form>
       <DevTool control={control} />
@@ -108,7 +203,7 @@ const YoutubeForm = () => {
 
 export default YoutubeForm;
 
-// how to react hook form 
+// how to react hook form
 
 // 1. Install and Import
 // Install with npm install react-hook-form
